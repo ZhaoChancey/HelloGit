01afa6b5d747d4764e0c64579a244052b2537263
ZhaoChancey <756359715@qq.com>
2020-07-03 18:24:49 +0800

    添加文件

---
+++ HelloGit/src/main/java/cn/bjtu/HelloGit.java
@@ -0,0 +1,11 @@
+package cn.bjtu;
+
+/**
+ * @author chancey
+ * @create 2020-07-03 17:13
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
